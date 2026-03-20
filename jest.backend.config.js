5b874f254b8400f254d3a2db72548b5ea212767d
Shauryan Agrawal <Shauryan100@gmail.com>
2026-03-20 15:05:49 +0800

    test(auth): add end-to-end integration coverage for register, login, and forgot-password flows

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -6,12 +6,12 @@ export default {
   testEnvironment: "node",
 
   // which test to run
-  testMatch: [
-    "<rootDir>/controllers/**/*.test.js",
-    "<rootDir>/helpers/**/*.test.js",
-    "<rootDir>/middlewares/**/*.test.js",
-    "<rootDir>/models/*.test.js",
-  ],
+testMatch: [
+  "<rootDir>/controllers/**/*.test.js",
+  "<rootDir>/controllers/**/*.integration.test.js",
+  "<rootDir>/helpers/**/*.test.js",
+  "<rootDir>/middlewares/**/*.test.js",
+],
 
   // jest code coverage
   collectCoverage: true,
