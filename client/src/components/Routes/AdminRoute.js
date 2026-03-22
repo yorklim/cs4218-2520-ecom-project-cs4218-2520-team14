dcd61d81e267f3a6b951e575ccd7ad6f47795aaf
jonasong <jonasong2@gmail.com>
2026-03-22 19:53:44 +0800

    revert changes to AdminRoute

--- client/src/components/Routes/AdminRoute.js
+++ client/src/components/Routes/AdminRoute.js
@@ -1,27 +1,28 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { useAuth } from "../../context/auth";
 import Spinner from "../Spinner";
 
 export default function AdminRoute() {
-  const [ok, setOk] = useState(true);
+  const [ok, setOk] = useState(false);
   const [auth, setAuth] = useAuth();
 
-  // useEffect(() => {
-  //   const authCheck = async () => {
-  //     try {
-  //       const res = await axios.get("/api/v1/auth/admin-auth");
-  //       if (res.data.ok) {
-  //         setOk(true);
-  //       } else {
-  //         setOk(false);
-  //       }
-  //     } catch (error) {
-  //       setOk(false);
-  //     }
-  //   };
-  //   if (auth?.token) authCheck();
-  // }, [auth?.token]);
+  useEffect(() => {
+    const authCheck = async () => {
+      try {
+        const res = await axios.get("/api/v1/auth/admin-auth");
+        if (res.data.ok) {
+          setOk(true);
+        } else {
+          setOk(false);
+        }
+      } catch (error) {
+        setOk(false);
+      }
+    };
+    if (auth?.token) authCheck();
+  }, [auth?.token]);
 
   return ok ? <Outlet /> : <Spinner />;
 }
